e8f0a34f37f4277318dbf44653f075d78016cadc
MachariaP <walburphinehas78@gmail.com>
2024-07-17 17:21:31 +0300

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,7 @@ with unique keys and retrieve it, suitable for caching and temporary data storag
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional
 
 
 class Cache:
@@ -38,3 +38,49 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(name=key, value=data)
         return key
+
+    """
+    Extends the Cache class to include methods for retrieving data from Redis and
+    converting it back to its original type. 
+    Implements get, get_str and get_int methods.
+    """
+    def get(self, key: str, fn: Optional[callable] = None) -> Union[str, bytes, int, float, None]:
+        
+        """
+        Retrieves data from Redis by key and converts it using the provided function.
+
+        Args:
+            key: The key of the data to retrieve.
+            fn: An optional callable to convert the data back to its desired format.
+
+        Returns:
+            The retrieved data, optionally converted using fn or None if key doesn't exist.
+        """
+        value = self._redis.get(name=key)
+        if value is not None and fn is not NOne:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrives a strinf from Redis.
+
+        Args:
+            key: The key of the data to retrieve.
+
+        Returns:
+            The retrieved string or None if key doesn't exist.
+        """
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieves an integer from Redis.
+
+        Args:
+            key: The key of the data to retrieve.
+
+        Returns:
+            The retrieved integer or None if key doesn't exist.
+        """
+        return self.get(key, fn=int)
